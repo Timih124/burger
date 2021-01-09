91129e166ed74704f71f467da4b1af7ae1ea810c
Timih124 <tim.hackel24@gmail.com>
2021-01-09 05:58:34 -0600

    updating server.js file

---
+++ models/burger.js
@@ -0,0 +1,3 @@
+// create the code that will call the ORM function
+
+var orm = require("../config/orm.js");
--- server.js
+++ server.js
@@ -2,6 +2,29 @@
 var express = require("express");
 var expressHb = require("express-handlebars")
 
-
-
+// creating servers
 var app = express()
+var PORT = porcess.env.PORT || 3000;
+
+//  static content for the app in the "public" directory from app directory.
+
+app.use(express.static("public"));
+
+// Parse application body as JSON
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+
+// creating handlebars engines
+app.engine("handlebars", expressHb({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// importing burger controllers route
+var routes = require("./controllers/burgers_controller.js")
+app.use(routes);
+
+// app begins to listen and sever starts
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
