8e67b6de1af16a11e134769848fc7ef4462a3b83
Timih124 <tim.hackel24@gmail.com>
2021-01-09 05:36:46 -0600

    adding config folder along with other js files

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// create dependecies with express and mysql
+var mysql = require("mysql");
+var express = require("express");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    PORT: 3306,
+    user: "root",
+    password: "password1",
+    database: "burgers_DB",
+    
+
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
